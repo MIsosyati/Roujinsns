0be519692fb47f13258ee04f623fa2c7a222ddfd
hiyoyo <omiso0803@gmail.com>
2021-06-05 17:17:56 +0900

    ファイヤーベース内容 日付取得追記

--- app/src/main/java/com/example/roujinsns/NikkiActivity.kt
+++ app/src/main/java/com/example/roujinsns/NikkiActivity.kt
@@ -1,13 +1,32 @@
 package com.example.roujinsns
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.LocalDateTime
+
 
 class NikkiActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nikki)
+        val textclock = findViewById<TextView>(R.id.textView6)
+        val localDateTime = LocalDate.now()
+        textclock.setText(localDateTime.toString())
+        val savebutton =findViewById<Button>(R.id.savebutton)
+        data class Task(val id:String, val name:String, val edittext:String,val date:String)
 
+        savebutton.setOnClickListener{
+
+        }
     }
+
+
 }
