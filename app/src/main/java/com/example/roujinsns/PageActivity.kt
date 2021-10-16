f71f890db150c80d0e8a0d64a1029d438bc35a62
hiyoyo <omiso0803@gmail.com>
2021-10-16 15:34:33 +0900

    kousinnyou

--- app/src/main/java/com/example/roujinsns/PageActivity.kt
+++ app/src/main/java/com/example/roujinsns/PageActivity.kt
@@ -2,10 +2,15 @@ package com.example.roujinsns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class PageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page)
+        val textView8 = findViewById<TextView>(R.id.textView8)
     }
+
+
+
 }
